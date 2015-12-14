c4b9e6ca013bdfbf9e30fe6a6ea6ff092dbb21ad
David Manning <dlmanning@me.com>
2015-12-13 22:43:47 -0800

    silly cjs hack

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib').default
+var mod = require('./lib')
+var instyled = mod.default
+
+instyled.instyledWithTransform = mod.instyledWithTransform
+instyled.hierarchical = mod.hierarchical
+instyled.flatKeyed = mod.flatKeyed
+
+module.exports = instyled
